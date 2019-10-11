ed01bb1c898b0bab5714672e54b0d43933ebfd53
张晨旭 <zhangxu1161@gmail.com>
2019-10-11 10:03:30 +0800

    feat: 链接拼接完成

--- src/helpers/url.ts
+++ src/helpers/url.ts
@@ -14,25 +14,35 @@ export  function bindUrl(url: string, params?: any) {
     return url
   }
   const parts: string[] = []
-  Object.keys(params).forEach((key) => {
-    let val = params[key]
+  const keys = Object.keys(params)
+  keys.forEach(key => {
+    let val: any = params[key]
     if (val === null || typeof val === 'undefined') {
       return
     }
-    let values: string[]
+    let values :string[]
     if (Array.isArray(val)) {
       values = val
-      key+= '[]'
+      key += '[]'
     } else {
       values = [val]
     }
-    values.forEach((val) => {
+    values.forEach(val => {
       if (isDate(val)) {
-        val = val.toString()
+        val = val.toISOString()
       } else if (isObj(val)) {
-        val= JSON.stringify(val)
+        val = JSON.stringify(val)
       }
       parts.push(`${encode(key)}=${encode(val)}`)
     })
   })
+  let serializedParams = parts.join('&')
+  if (serializedParams) {
+    const markIndex = url.indexOf('#')
+    if (markIndex !== -1) {
+      url = url.slice(0, markIndex)
+    }
+    url += (url.indexOf('?') === -1 ? '?' : '&') + serializedParams
+  }
+  return  url
 }
--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,6 +1,9 @@
-export function isObj (val: any): boolean{
+const toString = Object.prototype.toString
 
+export function isObj (val: any): val is Object{
+  return  val !== null && typeof val === 'object'
 }
-export  function isDate(val: any): boolean {
+export  function isDate(val: any): val is Date {
+  return toString.call(val) === '[object Date]'
+}
 
-}
